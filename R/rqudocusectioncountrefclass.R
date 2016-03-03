de6d8d789c49cc4322fa8f3881ae2a6cd3e20cd4
charlotte-ngs <ngs.charlotte@gmail.com>
2016-03-03 07:22:48 +0100

    Added devtools and roxygen2 to external packages in packrat and added documentation on counter ref classes

--- R/rqudocusectioncountrefclass.R
+++ R/rqudocusectioncountrefclass.R
@@ -7,22 +7,50 @@
 ### ################################################### ###
 
 
-#' Reference Class for section count
+#' Reference Class for section counts
 #'
-#' The class stores counts for three levels of sections.
-#' The field sHash contains the hash signs that are taken
-#' from an rmarkdown section title. Based on the number of
-#' hash signs the corresponding counter is incremented
+#' @description
+#' A reference object of reference class \code{SectionCount}
+#' represents the numbers in front of a section title.
+#'
+#' @details
+#' The section title number  counts the numbers of different
+#' section at any given level up and until a given section
+#' title. In a markdown (md) document section levels
+#' of titles are denoted by hash (#) signs. Based on the
+#' number of hash signs of a given section title, the level of
+#' the corresponding section title can be inferred. The more
+#' hash signs the lower the level of the section title. Hence
+#' one hash means top-level section title, two hashes stand
+#' for subsections, three hashes denote subsubsectiones, etc.
+#' For a given section title the level determines the corresponding
+#' number of the section title. For a top-level section there
+#' is just one number, for a subsection there are two numbers
+#' separated by a dot (.) and for subsubsections there are
+#' three numbers all separated by dots. Each of the numbers
+#' that are associated with a given section title count the
+#' number of sections for a specific level up
+#' and until that given section title.
+#'
+#' @field vSectionCount   vector with section counts
 sectionCount <- setRefClass(Class   = "SectionCount",
-                            fields  = list(nSectionCount       = "numeric",
+                            fields  = list(vSectionCount       = "numeric",
+                                           nSectionCount       = "numeric",
                                            nSubSectionCount    = "numeric",
                                            nSubSubsectionCount = "numeric",
                                            sHash               = "character",
                                            sCountSep           = "character",
                                            nNrExtraHash        = "numeric"),
                             methods = list(
+                              .lGetDefaults = function(){
+                                'default values for given reference object'
+                                return(list(nNrSectionLevels = 6L))
+                              }
                               initialize = function(){
                                 'Initialize count fields and set default for count separator'
+                                lRefObjDefaults     <- .self$.lGetDefaults()
+                                vSectionCount       <<- vector(mode = "numeric",
+                                                               length = lRefObjDefaults$nNrSectionLevels)
                                 nSectionCount       <<- 0
                                 nSubSectionCount    <<- 0
                                 nSubSubsectionCount <<- 0
@@ -37,9 +65,19 @@ sectionCount <- setRefClass(Class   = "SectionCount",
                               },
                               incrSectionCounts = function(){
                                 'Increment section counts based on number of hash signs'
+                                # count the number of hash signs in sHash
                                 nNrHash <- nchar(sHash)
+                                # subtract any number of extra hashes
                                 if (nNrExtraHash < nNrHash)
                                   nNrHash <- nNrHash - nNrExtraHash
+                                # increment the section count at the appropriate
+                                #  level
+                                if (nNrHash > length(vSectionCount))
+                                  stop(" * Current title seams to have ", nNrHash, " levels. The max. number of levels is: ",
+                                        length(vSectionCount))
+                                vSectionCount[nNrHash] <- vSectionCount[nNrHash] + 1
+                                # set all counts of lower levels to 0
+                                if (nNrHash < )
                                 if (nNrHash == 3){
                                   nSubSubsectionCount <<- nSubSubsectionCount + 1
                                 } else if(nNrHash == 2){
@@ -83,8 +121,21 @@ sectionCount <- setRefClass(Class   = "SectionCount",
 #' The main method of a SectionEnumerator reference object
 #' is \code{displayNumSection}. This takes as input a string
 #' of an unnumbered section, parses that string and computes
-#' the section number based on the number of hashes.
+#' the section number based on the number of hashes. For some
+#' reason, the option \code{number_sections = FALSE} does not
+#' work for top level sections, but it only works for subsections.
+#' As a work-around, all sections are moved down one level to subsections
+#' by ignoring a given number of \code{nNrExtraHash} hash signs.
+#' Hence a subsection will be numbered as a section and a subsubsection
+#' will be numbered as a subsection etc.
 #'
+#' @field sUnNumSection       original ununmbered section string
+#' @field rcSectionCount      reference object representing section count
+#' @field sHash               hashes of caption
+#' @field sRemCaption         remaining caption after removing hashes
+#' @field sNumCaptionResult   resulting caption string with number
+#' @field sSectionSplit       split character between number and title
+#' @field nNrExtraHash        number of extra hashes not used for section counts
 #' @export SectionEnumerator
 #' @exportClass SectionEnumerator
 SectionEnumerator <- setRefClass(Class   = "SectionEnumerator",
@@ -128,9 +179,10 @@ SectionEnumerator <- setRefClass(Class   = "SectionEnumerator",
                                                                  sRemCaption)
 
                                    },
-                                   displayNumSection = function(psUnNumSection = NULL, pnNrExtraHash = 0){
+                                   displayNumSection = function(psUnNumSection = NULL, pnNrExtraHash = NULL){
                                      'Parsing of unnumbered section string and display the string including the section number'
-                                     nNrExtraHash <<- pnNrExtraHash
+                                     if (!is.null(pnNrExtraHash))
+                                       nNrExtraHash <<- pnNrExtraHash
                                      if (!is.null(psUnNumSection))
                                        sUnNumSection <<- psUnNumSection
                                      .self$parseUnNumSection()
